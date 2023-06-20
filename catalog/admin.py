2f7378802527ec6b0d15fe5d0ee8b3fa7176b564
jezyk_we_mgle <anastasiafabricnaama@gmail.com>
2023-06-20 13:55:09 +0300

    created admin view

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,2 +1,22 @@
 from django.contrib import admin  # noqa: F401
-# Register your models here.
+from .models import City, Product, Supplier, Client
+
+
+@admin.register(City)
+class CityAdmin(admin.ModelAdmin):
+    list_display = ['name', 'state', 'postcode']
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['product_name', 'description', 'price', 'supplier', 'active', 'created_at']
+
+
+@admin.register(Client)
+class ClientAdmin(admin.ModelAdmin):
+    list_display = ['name', 'surname', 'email', 'city', 'get_products']
+
+
+@admin.register(Supplier)
+class SupplierAdmin(admin.ModelAdmin):
+    list_display = ['title', 'email', 'phone_number', 'city']
