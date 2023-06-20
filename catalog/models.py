6e9bd743c01b474ca9c01678981ffc59b1376ff8
jezyk_we_mgle <anastasiafabricnaama@gmail.com>
2023-06-20 14:02:13 +0300

    some changes

--- catalog/models.py
+++ catalog/models.py
@@ -1,6 +1,7 @@
 from django.core.validators import RegexValidator
 from django.db import models
 from django.urls import reverse
+from django.contrib import admin
 
 
 class City(models.Model):
@@ -8,7 +9,7 @@ class City(models.Model):
 
     name = models.CharField(max_length=200)
     state = models.CharField(max_length=200)
-    postcode = models.IntegerField(help_text="Enter the postcode of the city")
+    postcode = models.CharField(max_length=20, help_text="Enter the postcode of the city")
 
     def __str__(self):
         """String for representing the Model object (in Admin site etc.)"""
@@ -19,13 +20,33 @@ class City(models.Model):
         return reverse('city-detail', args=[str(self.id)])
 
 
+class Supplier(models.Model):
+    """Model representing a supplier"""
+
+    code = models.CharField(max_length=10, help_text='Enter the vendor`s or producer`s identifier')
+    title = models.CharField(max_length=255)
+    email = models.EmailField(max_length=255)
+    phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$',
+                                 message="Phone number must be entered in the format: '+999999999'.")
+    phone_number = models.CharField(validators=[phone_regex], max_length=17, blank=True)
+    city = models.OneToOneField(City, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        """String for representing the Model object (in Admin site etc.)"""
+        return f'{self.title} ({self.city})'
+
+    def get_absolute_url(self):
+        """Returns the url to access a particular supplier."""
+        return reverse('supplier-detail', args=[str(self.id)])
+
+
 class Product(models.Model):
     """Model representing a product"""
 
     product_name = models.CharField(max_length=255)
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
-    vendor_code = models.CharField(max_length=255, help_text="Enter the vendor`s or producer`s identifier ")
+    supplier = models.ForeignKey(Supplier, on_delete=models.SET_NULL, null=True)
     active = models.BooleanField()
     created_at = models.DateTimeField(auto_now_add=True)
 
@@ -47,7 +68,7 @@ class Client(models.Model):
     surname = models.CharField(max_length=200)
     email = models.EmailField(max_length=255)
     city = models.ForeignKey(City, on_delete=models.SET_NULL, null=True)
-    product = models.ManyToManyField(Product)
+    products = models.ManyToManyField(Product)
 
     def __str__(self):
         """String for representing the Model object (in Admin site etc.)"""
@@ -57,21 +78,7 @@ class Client(models.Model):
         """Returns the url to access a particular client."""
         return reverse('client-detail', args=[str(self.id)])
 
+    @admin.display(description='')
+    def get_products(self):
+        return ", ".join([p.product_name for p in self.products.all()])
 
-class Supplier(models.Model):
-    """Model representing a supplier"""
-
-    title = models.CharField(max_length=255)
-    email = models.EmailField(max_length=255)
-    phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$',
-                                 message="Phone number must be entered in the format: '+999999999'.")
-    phone_number = models.CharField(validators=[phone_regex], max_length=17, blank=True)
-    city = models.OneToOneField(City, on_delete=models.SET_NULL, null=True)
-
-    def __str__(self):
-        """String for representing the Model object (in Admin site etc.)"""
-        return f'{self.title} ({self.city})'
-
-    def get_absolute_url(self):
-        """Returns the url to access a particular supplier."""
-        return reverse('supplier-detail', args=[str(self.id)])
