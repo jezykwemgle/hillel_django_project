0bb086aea932b0eac6c46b8f10baf7482618e743
jezyk_we_mgle <anastasiafabricnaama@gmail.com>
2023-06-27 14:16:06 +0300

    added form

---
+++ triangle/forms.py
@@ -0,0 +1,19 @@
+from django import forms
+from django.core.exceptions import ValidationError
+
+
+class TriangleForm(forms.Form):
+    cathetus1 = forms.IntegerField(required=True)
+    cathetus2 = forms.IntegerField(required=True)
+
+    def get_cathetus1(self):
+        cathetus1 = self.cleaned_data.get('cathetus1')
+        if cathetus1 <= 0:
+            raise ValidationError("Catheters must be greater than 0")
+        return cathetus1
+
+    def get_cathetus2(self):
+        cathetus2 = self.cleaned_data.get('cathetus2')
+        if cathetus2 <= 0:
+            raise ValidationError("Catheters must be greater than 0")
+        return cathetus2
