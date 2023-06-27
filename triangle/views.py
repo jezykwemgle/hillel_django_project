6386e951b7db0d310608bedba181aca9c924ebb4
jezyk_we_mgle <anastasiafabricnaama@gmail.com>
2023-06-27 14:16:40 +0300

    added view

--- triangle/views.py
+++ triangle/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .forms import TriangleForm
+from math import sqrt, pow
 
-# Create your views here.
+
+def triangle_hypotenuse(request):
+    if request.method == 'POST':
+        form = TriangleForm(request.POST)
+        if form.is_valid():
+            c1 = form.get_cathetus1()
+            c2 = form.get_cathetus2()
+            hypotenuse = sqrt(pow(c1, 2) + pow(c2, 2))
+            return render(request, 'hypotenuse.html', {'form': form, 'hypotenuse': hypotenuse})
+    else:
+        form = TriangleForm()
+        return render(request, 'hypotenuse.html', {'form': form})
