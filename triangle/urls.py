e3a33abd589488116a656d831a7c3524306624ed
jezyk_we_mgle <anastasiafabricnaama@gmail.com>
2023-06-28 13:11:26 +0300

    added paths

--- triangle/urls.py
+++ triangle/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
 
-from .views import triangle_hypotenuse
+from . import views
 
 urlpatterns = [
-    path('', triangle_hypotenuse, name='triangle_hypotenuse')
+    path('', views.view_main_paige, name='main_paige'),
+    path('triangle/', views.triangle_hypotenuse, name='triangle_hypotenuse'),
+    path('person/', views.create_person, name='create_person'),
+    path('person/<int:pk> ', views.update_person, name='update_person'),
+    path('person-detail/<int:pk>', views.person_detail, name="person_detail"),
+    path('person-list/', views.all_person, name='all_person')
 ]
